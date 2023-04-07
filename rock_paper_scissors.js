3ac83c0f022d8f2f183c2a76828776a58c29d238
Miguel Torres <migueltorres2307@gmail.com>
2023-04-07 12:48:21 -0700

    Created playRound function

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,14 +1,39 @@
-let computerChoice = '';
-
-function getComputerChoice (min, max){
-    return Math.floor(Math.random() * 3) + 1;
+function getComputerChoice (_min, max){
+    return Math.floor(Math.random() * max) + 1
 }
 
-if (getComputerChoice === 1){
-    computerChoice = 'Rock';
+let computerSelection = getComputerChoice(1, 3);
+    if (computerSelection == 1){
+        computerSelection = 'rock';
+    } else if (computerSelection == 2){
+        computerSelection = 'paper';
+    } else if (computerSelection == 3){
+        computerSelection = 'scissors';
+    }
+
+let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == 'rock' && computerSelection == 'scissors'){
+        return "You win! Rock beats Scissors";
+
+    } else if (playerSelection == 'paper' && computerSelection == 'rock'){
+        return "You win! Paper beats Rock";
+
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper'){
+        return "You win! Scissors beats Paper";
+
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock'){
+        return "You lose. Rock beats Scissors";
+
+    } else if (playerSelection == 'rock' && computerSelection == 'paper'){
+        return "You lose. Paper beats Rock";
+
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors'){
+        return "You lose. Scissors beats Paper";
+    } else {
+        return "Its a tie!"
+    }
 }
-else if (getComputerChoice === 2){
-    computerChoice = 'Paper';
-}
-else
-    computerChoice = 'Scissors';
+
+console.log(playRound(playerSelection, computerSelection));
